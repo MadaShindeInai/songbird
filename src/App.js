18440edba867cc3681837088b6ec250a2eedf0c9
MadaShindeInai <1992@sergei.by>
2020-02-10 11:13:35 +0300

    feat: add App iand index component logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Quiz from './components/Quiz';
+import Options from './components/Options';
+import GameInfo from './components/GameInfo';
+import TheEnd from './components/TheEnd';
+import './styles/main.scss';
+import BirdsData from './utils/BirdsData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const randomBird = birds => birds[Math.floor(Math.random() * birds.length)];
+  const [score, setScore] = useState(0);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [currentBird, setCurrentBird] = useState(
+    randomBird(BirdsData[currentStep].birds)
   );
-}
+  const [state, setState] = useState(true);
+  const [tempBird, setTempBird] = useState('');
+  const [theEnd, setTheEnd] = useState(false);
+  console.log('TCL: App -> tempBird', tempBird);
+
+  const goNextLevel = () => {
+    if (currentStep < BirdsData.length - 1) {
+      setCurrentStep(currentStep + 1);
+      setCurrentBird(randomBird(BirdsData[currentStep + 1].birds));
+    } else {
+      setTheEnd(true);
+    }
+    setState(true);
+  };
+
+  return (
+    <>
+      {theEnd ? (
+        <TheEnd />
+      ) : (
+          <div className="wrapper">
+            <div>
+              <Header
+                BirdsData={BirdsData}
+                score={score}
+                currentStep={currentStep}
+              />
+              <Quiz currentBird={currentBird} state={state} />
+              <div className="game">
+                <Options
+                  BirdsDataItem={BirdsData[currentStep]}
+                  currentBird={currentBird}
+                  setState={setState}
+                  state={state}
+                  setScore={setScore}
+                  score={score}
+                  setTempBird={setTempBird}
+                />
+                <GameInfo
+                  currentBird={currentBird}
+                  tempBird={tempBird}
+                  state={state}
+                />
+              </div>
+              <button
+                type="button"
+                onClick={goNextLevel}
+                className="btn"
+                disabled={state}
+              >
+                Next Level
+            </button>
+            </div>
+          </div>
+        )}
+    </>
+  );
+};
 
 export default App;
