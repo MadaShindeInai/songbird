b24f4bc5dc9e3d9961fb00cac50c586778ca955e
MadaShindeInai <1992@sergei.by>
2020-02-16 17:29:43 +0300

    feat: add project deployment to gh-pages

---
+++ precache-manifest.2c47a24aad2f36e631add46e1e819489.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a89792869b88ea6cd9ba021ebe7dadc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f838d40145faeeadd05",
+    "url": "./static/css/2.0036de3e.chunk.css"
+  },
+  {
+    "revision": "b3388df6112c1358e3da",
+    "url": "./static/css/main.bd3b3cd4.chunk.css"
+  },
+  {
+    "revision": "5f838d40145faeeadd05",
+    "url": "./static/js/2.a726d049.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.a726d049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3388df6112c1358e3da",
+    "url": "./static/js/main.8aab0b4c.chunk.js"
+  },
+  {
+    "revision": "d3b58900b9514c34972d",
+    "url": "./static/js/runtime-main.dfc26e67.js"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "./static/media/bird.06a46938.jpg"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "./static/media/logo.4f82cd73.svg"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "./static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "./static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "d07ffa04b3f14e723afa8f214d0b89f2",
+    "url": "./static/media/sertif.d07ffa04.pdf"
+  },
+  {
+    "revision": "169a3a76c7f8ae0911b6ea9fe098d000",
+    "url": "./static/media/success.169a3a76.wav"
+  },
+  {
+    "revision": "5acd1a089942ad6d903b600a76ef063c",
+    "url": "./static/media/wrong.5acd1a08.mp3"
+  }
+]);
